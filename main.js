82f216d15a183ec1c796e7a245c6225a42ef61ad
Jose Daniel <josopu96@gmail.com>
2019-04-07 21:20:24 +0200

    Integrations btw Angular 2 and ElectronJS #71

--- main.js
+++ main.js
@@ -1,61 +1,74 @@
-const electron = require('electron')
-const { app, BrowserWindow } = electron
 
-const path = require('path')
-const url = require('url')
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var electron_1 = require("electron");
+var path = require("path");
+var url = require("url");
+var win, serve;
+var args = process.argv.slice(1);
+serve = args.some(function (val) { return val === '--serve'; });
 
-
-
-
-const express = require('express');
-const bodyParser = require('body-parser');
-const server = express();
-const usuario = require('./routes/usuario.route'); // Imports routes for the products
-
-// Set up mongoose connection
-const mongoose = require('mongoose');
-let dev_db_url = 'mongodb+srv://admin:admin@frankfurtcluster-kdmjy.mongodb.net/VeterinApp';
-let mongoDB = process.env.MONGODB_URI || dev_db_url;
-mongoose.connect(mongoDB);
-mongoose.Promise = global.Promise;
-let db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-
-server.use(bodyParser.json());
-server.use(bodyParser.urlencoded({extended: false}));
-server.use('/usuarios', usuario);
-
-let port = 9018;
-server.listen(port, () => {
-  console.log('Server is up and running on port numner ' + port);
-});
-
-
-
-
-
-
-
-let win
 
 function createWindow() {
-    win = new BrowserWindow({ width: 1366, height: 768})
-    // win.maximize()
-    win.webContents.openDevTools()
-    win.loadURL(url.format({
-        pathname: path.join(__dirname, '/components/index/index.html'),
-        protocol: 'file',
-        slashes: true
-    }))
+  var electronScreen = electron_1.screen;
+  var size = electronScreen.getPrimaryDisplay().workAreaSize;
+  // Create the browser window.
+  win = new electron_1.BrowserWindow({
+      x: 0,
+      y: 0,
+      width: size.width,
+      height: size.height,
+      webPreferences: {
+          nodeIntegration: true,
+      },
+  });
+  if (serve) {
+      require('electron-reload')(__dirname, {
+          electron: require(__dirname + "/node_modules/electron")
+      });
+      win.loadURL('http://localhost:4200');
+  }
+  else {
+      win.loadURL(url.format({
+          pathname: path.join(__dirname, 'dist/index.html'),
+          protocol: 'file:',
+          slashes: true
+      }));
+  }
+  if (serve) {
+      win.webContents.openDevTools();
+  }
+  // Emitted when the window is closed.
+  win.on('closed', function () {
+      // Dereference the window object, usually you would store window
+      // in an array if your app supports multi windows, this is the time
+      // when you should delete the corresponding element.
+      win = null;
+  });
 }
-
-exports.openWindow = () => {
-    let newWin = new BrowserWindow({ width: 400, height: 200 })
-    newWin.loadURL(url.format({
-        pathname: path.join(__dirname, '/components/enupal/enupal.html'),
-        protocol: 'file',
-        slashes: true
-    }))
+try {
+  // This method will be called when Electron has finished
+  // initialization and is ready to create browser windows.
+  // Some APIs can only be used after this event occurs.
+  electron_1.app.on('ready', createWindow);
+  // Quit when all windows are closed.
+  electron_1.app.on('window-all-closed', function () {
+      // On OS X it is common for applications and their menu bar
+      // to stay active until the user quits explicitly with Cmd + Q
+      if (process.platform !== 'darwin') {
+          electron_1.app.quit();
+      }
+  });
+  electron_1.app.on('activate', function () {
+      // On OS X it's common to re-create a window in the app when the
+      // dock icon is clicked and there are no other windows open.
+      if (win === null) {
+          createWindow();
+      }
+  });
 }
-
-app.on('ready', createWindow)
+catch (e) {
+  // Catch Error
+  // throw e;
+}
+//# sourceMappingURL=main.js.map

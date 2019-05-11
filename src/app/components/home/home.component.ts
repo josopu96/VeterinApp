7365531f09eafa554173d7c3cf7513bfa6816069
Jose Daniel <josopu96@gmail.com>
2019-05-11 18:02:47 +0200

    [#28] Styling code

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -17,53 +17,49 @@ export class HomeComponent implements OnInit {
   hora: number;
   mensajeBienvenida: String;
   time: Date = new Date();
-  now:string=('0'+this.time.getHours()).substr(-2) + ('0'+this.time.getMinutes()).substr(-2) + ('0'+this.time.getSeconds()).substr(-2);
+  now: string = ('0' + this.time.getHours()).substr(-2) + ('0' + this.time.getMinutes()).substr(-2) + ('0' + this.time.getSeconds()).substr(-2);
   constructor(
     private cookieService: CookieService,
     private router: Router,
     private globalService: GlobalService
   ) {
-    console.log("el usuario tiene token: "+this.cookieService.get("token"));
     const token = this.cookieService.get("token");
     const isUserLogged = token ? true : false;
-    console.log("isUserLogged: "+isUserLogged);
-    console.log("token: "+token);
     if (!isUserLogged) {
       this.router.navigateByUrl("/login");
     }
   }
 
   ngOnInit() {
-    this.tema = "_"+this.globalService.getTema();
-    console.log("el nombre del usuario es: "+this.globalService.getUsuario().nombre);
+    this.tema = "_" + this.globalService.getTema();
     this.nombreUsuario = this.globalService.getUsuario().nombre;
-    if(this.time.getHours()<12 && this.time.getHours() >= 3){
+    if (this.time.getHours() < 12 && this.time.getHours() >= 3) {
       this.setBienvenida('M');
-    } else if(this.time.getHours()<20 && this.time.getHours()>=2){
+    } else if (this.time.getHours() < 20 && this.time.getHours() >= 2) {
       this.setBienvenida('T');
     } else {
       this.setBienvenida('N');
     }
-    let timeoutId = setInterval(() => {        
+    let timeoutId = setInterval(() => {
       this.time = new Date();
-      this.now = ('0'+this.time.getHours()).substr(-2) + ('0'+this.time.getMinutes()).substr(-2) + ('0'+this.time.getSeconds()).substr(-2);
-      if(this.time.getHours()<12 && this.time.getHours() >= 3){
+      this.now = ('0' + this.time.getHours()).substr(-2) + ('0' + this.time.getMinutes()).substr(-2) + ('0' + this.time.getSeconds()).substr(-2);
+      if (this.time.getHours() < 12 && this.time.getHours() >= 3) {
         this.setBienvenida('M');
-      } else if(this.time.getHours()<20 && this.time.getHours()>=2){
+      } else if (this.time.getHours() < 20 && this.time.getHours() >= 2) {
         this.setBienvenida('T');
       } else {
         this.setBienvenida('N');
       }
     }, 1000);
   }
- 
-  setBienvenida(estado:string){
-    if(estado=='M'){
-      this.mensajeBienvenida="Buenos días, ";
-    } else if(estado=='T'){
-      this.mensajeBienvenida="Buenas tardes, ";
+
+  setBienvenida(estado: string) {
+    if (estado == 'M') {
+      this.mensajeBienvenida = "Buenos días, ";
+    } else if (estado == 'T') {
+      this.mensajeBienvenida = "Buenas tardes, ";
     } else {
-      this.mensajeBienvenida="Buenas noches, ";
+      this.mensajeBienvenida = "Buenas noches, ";
     }
   }
 }

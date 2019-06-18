e2f0cd58356109cfd958c9930db470f88852db9b
Jose Daniel <josopu96@gmail.com>
2019-06-18 09:13:54 +0200

    [#41] Backend para creación de tratamientos para una mascota

--- src/server/routes/mascota.route.js
+++ src/server/routes/mascota.route.js
@@ -8,5 +8,7 @@ router.get('/', cors(), mascota_controller.getMascotas);
 router.get('/:id', cors(), mascota_controller.getMascota);
 router.post('/create', cors(), mascota_controller.createMascota);
 router.post('/:id/update', cors(), mascota_controller.updateMascota);
+router.get('/:id/tratamientos', cors(), mascota_controller.getTratamientos);
+router.post('/addTratamiento', cors(), mascota_controller.crearTratamiento);
 
 module.exports = router;

873eaabaab7ac0fbe97d4474d739fa8f4df90a65
Javier <javier21roga@gmail.com>
2019-06-23 17:40:43 +0200

    [#45] Últimos ajustes del resumen de mascotas
    Se han añadido las operaciones a la tabla.
    Se han ordenador las operaciones por fecha descendente.
    Se ha corregido un error al mostrar la fecha de fallecimiento.
    Se ha quitado la fecha de alta, ya que no disponemos de ella en la base de datos.
    Se han ajustado los estilos.

--- src/app/components/mascotas/resumen/resumen.component.ts
+++ src/app/components/mascotas/resumen/resumen.component.ts
@@ -3,7 +3,7 @@ import { CookieService } from 'ngx-cookie-service';
 import { Router, ActivatedRoute } from '@angular/router';
 import { GlobalService } from '../../../services/globalService';
 import { CabeceraTabla, OperacionResumenMascota } from '../../../models/tablas';
-import { Mascota, Cliente } from '../../../app.dataModels';
+import { Mascota, Cliente, Tratamiento } from '../../../app.dataModels';
 
 @Component({
   selector: 'app-resumen',
@@ -36,7 +36,9 @@ export class ResumenComponent implements OnInit {
     this.tema = "_" + this.globalService.getTema();
     if (this.idMascota) {
       this.mascota = this.globalService.getMascotaPorId(this.idMascota);
+      console.log(this.elements);
       if (this.mascota) {
+        this.inicializaElementosTabla();
         if (this.mascota.idCliente) {
           this.cliente = this.globalService.getClientePorId(this.mascota.idCliente);
         }
@@ -46,6 +48,60 @@ export class ResumenComponent implements OnInit {
     }
 
     this.ready = true;
+  }
+
+  inicializaElementosTabla() {
+    //Incluimos los tratamientos en el resumen de la mascota
+    this.mascota.tratamientos.forEach(tratamiento => {
+      let element: OperacionResumenMascota = new OperacionResumenMascota;
+      element.id = tratamiento._id;
+      element.fecha = tratamiento.fecha;
+      element.tipo = 'Tratamiento';
+      element.descripcion = tratamiento.tipoTratamiento;
+      this.elements.push(element);
+    });
+    //Incluimos las vacunas en el resumen de la mascota
+    this.mascota.vacunas.forEach(vacuna => {
+      let element: OperacionResumenMascota = new OperacionResumenMascota;
+      element.id = vacuna._id;
+      element.fecha = vacuna.fecha;
+      element.tipo = 'Vacuna';
+      element.descripcion = vacuna.tipoVacuna;
+      this.elements.push(element);
+    });
+    //Incluimos las pruebas en el resumen de la mascota
+    this.mascota.pruebas.forEach(prueba => {
+      let element: OperacionResumenMascota = new OperacionResumenMascota;
+      element.id = prueba._id;
+      element.fecha = prueba.fecha;
+      element.tipo = 'Prueba';
+      element.descripcion = prueba.tipoPrueba;
+      this.elements.push(element);
+    });
+    //Incluimos las analíticas en el resumen de la mascota
+    this.mascota.analiticas.forEach(analitica => {
+      let element: OperacionResumenMascota = new OperacionResumenMascota;
+      element.id = analitica._id;
+      element.fecha = analitica.fecha;
+      element.tipo = 'Analítica';
+      element.descripcion = analitica.descripcion;
+      this.elements.push(element);
+    });
+    //Incluimos las desparasitaciones en el resumen de la mascota
+    this.mascota.desparasitaciones.forEach(desparasitacion => {
+      let element: OperacionResumenMascota = new OperacionResumenMascota;
+      element.id = desparasitacion._id;
+      element.fecha = desparasitacion.fecha;
+      element.tipo = 'Desparasitación';
+      element.descripcion = desparasitacion.tipoDesparasitacion;
+      this.elements.push(element);
+    });
+    
+    this.elements.sort((a, b): number => {
+      if (a.fecha > b.fecha) return -1;
+      if (a.fecha < b.fecha) return 1;
+      return 0;
+    })
   }
 
   inicializaCabecera() {
@@ -73,12 +129,12 @@ export class ResumenComponent implements OnInit {
     this.cliente = new Cliente();
     this.cliente.nombre = "Luis Sergio";
     this.cliente.apellidos = "García de todos los Santos";
-    for(let i = 0; i<20; i++){
+    for (let i = 0; i < 20; i++) {
       let operacion = new OperacionResumenMascota;
-      operacion.id = ""+i;
+      operacion.id = "" + i;
       operacion.fecha = new Date();
       operacion.tipo = "tratamiento";
-      operacion.descripcion = i+" Mg de Amoxicilina / 12 h";
+      operacion.descripcion = i + " Mg de Amoxicilina / 12 h";
       this.elements.push(operacion);
     }
   }
